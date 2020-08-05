5852c135cd2cb10b42c454ddd389633af0af2654
Marta Sobota <martazaryn@gmail.com>
2020-08-05 19:16:06 +0200

    WIP

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
-from bs4 import BeautifulSoup
 from requests_html import HTMLSession
-
-from secrets import USER, PASSWORD, LOGIN_WEBSITE
-
+from secrets import USER, PASSWORD, LOGIN_WEBSITE, WEBSITE
 
 sess = HTMLSession()
-
 
 payload = {
     "staff_username": USER,
     "staff_password": PASSWORD,
-    "send": 1
 }
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0"
 }
 
-r = sess.get(LOGIN_WEBSITE)
+r = sess.post(LOGIN_WEBSITE, data=payload, headers=headers)
 r.html.render()
 
-r = sess.post(LOGIN_WEBSITE, data=payload, headers=headers)
+# r = sess.get(WEBSITE)  # "http://www.campingshop.pl/panel?key=vifs5khr"
 
-for h in r.history:
-    print(h.text)
-print(r.html.links)
-print(r.status_code)
+r = sess.get("https://www.campingshop.pl/panel")
 
-# print(r.html.find("#orders"))
-# print(r.html.find("#orders"))
-# test = soup.find("div", class_="orders")
+print(r.html)
+
+print(r.html.find(".ui-jqgrid-sortable"))  # sprawdzam czy jestem zalogowana na stronie (dostaję pustą listę)
+print(r.html.find("#jqgh_last-orders_opt"))
